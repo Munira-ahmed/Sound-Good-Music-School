d589c1cdb20dd3510eb77dfbc9ff2f337c9d4bf7
NataliAlhallak <132365555+NataliAlhallak@users.noreply.github.com>
2023-11-28 15:20:23 +0100

    hopefully final

--- sound.sql
+++ sound.sql
@@ -1,37 +1,15 @@
-CREATE TYPE "lesson_level" AS ENUM ('beginner', 'intermediate', 'advanced');
+CREATE TYPE "level" AS ENUM ('beginner', 'intermediate', 'advanced');
 CREATE TYPE "instrument_type" AS ENUM ('wind', 'string', 'percussion', 'brass', 'keyboard', 'electric', 'vocal', 'woodwind', 'synthesizer', 'folk', 'world', 'electronic', 'other');
 CREATE TYPE "lesson_type" AS ENUM ('individual', 'group', 'ensemble');
 CREATE TYPE "ensemble_genre" AS ENUM ('jazz ensemble', 'classical orchestra', 'folk group', 'rock band', 'pop ensemble', 'blues group', 'country band', 'world music ensemble', 'hip-hop collective', 'reggae band', 'indie rock ensemble', 'electronic music group', 'experimental ensemble');
 
-CREATE TABLE "ensemble_genre" (
-    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
-    "value" VARCHAR(50) UNIQUE
-);
-
-
-CREATE TABLE "instrument_type" (
-    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
-    "value" VARCHAR(10) NOT NULL UNIQUE
-);
 
 
 CREATE TABLE "instrument" (
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     "quantity_in_stock" INT,
     "brand" VARCHAR(200),
-    "type_id" INT NOT NULL REFERENCES "instrument_type"
-);
-
-
-CREATE TABLE "lesson_type" (
-    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
-    "value" VARCHAR(50) UNIQUE
-);
-
-
-CREATE TABLE "level" (
-    "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
-    "value" VARCHAR(50) NOT NULL UNIQUE
+    "type_id" "instrument_type" NOT NULL 
 );
 
 
@@ -71,7 +49,7 @@ CREATE TABLE "contact_person" (
 CREATE TABLE "discount" (
     "student_id" INT NOT NULL REFERENCES "student" ON DELETE CASCADE,
     "siblings_id" VARCHAR(10),
-    "discount_percentage" DECIMAL(10),
+    "discount_percentage" DECIMAL(10)
 );
 
 
@@ -96,7 +74,7 @@ CREATE TABLE "person_phone" (
 
 
 CREATE TABLE "rental" (
-    "student_id" INT NOT NULL ON DELETE CASCADE,
+    "student_id" INT NOT NULL REFERENCES "student" ON DELETE CASCADE,
     "start_of_rental" TIMESTAMP(6) NOT NULL,
     "end_of_rental" TIMESTAMP(6) NOT NULL,
     "price" FLOAT(20) NOT NULL
@@ -119,27 +97,6 @@ CREATE TABLE "schedule" (
 );
 
 
-CREATE TABLE "student_fee" (
-   "student_id" INT NOT NULL REFERENCES "student" ON DELETE CASCADE,
-    "fee_date" TIMESTAMP(6),
-    "amount" FLOAT(10),
-    "bank_info" VARCHAR(50),
-    "status" VARCHAR(50),
-    "schedule_id" INT NOT NULL
-);
-
-
-
-CREATE TABLE "instructor_payment" (
-    "instructor_id" INT NOT NULL REFERENCES "instructor" ON DELETE CASCADE, 
-    "payment_date" TIMESTAMP(6),
-    "amount" FLOAT(10),
-    "bank_info" VARCHAR(50),
-    "schedule_id" INT NOT NULL REFERENCES "schedule"
-);
-
-
-
 CREATE TABLE "lesson" (
    "lesson_id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
    "description" VARCHAR(2000) NOT NULL,
@@ -148,14 +105,15 @@ CREATE TABLE "lesson" (
 
 
 
-
 CREATE TABLE "pricing_scheme" (
     "price_id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     "lesson_id" INT NOT NULL REFERENCES "lesson",
     "price" FLOAT(20),
-    "level_id" INT NOT NULL REFERENCES "level",
-    "lesson_type" FLOAT(10) NOT NULL REFERENCES "lesson_type"
+    "lesson_level" "level" NOT NULL,
+    "lesson_type" "lesson_type" NOT NULL  
 );
+
+
 
 
 
@@ -163,14 +121,14 @@ CREATE TABLE "ensembles" (
    "lesson_id" INT NOT NULL REFERENCES "lesson" ON DELETE CASCADE,
     "minimum_nr_of_students" INT NOT NULL,
     "maximum_nr_of_students" INT NOT NULL,
-    "ensemble_genre" INT NOT NULL REFERENCES "ensamble_genre"
+    "ensemble_genre" "ensemble_genre" NOT NULL 
 );
 
 
 
 CREATE TABLE "group_lesson" (
     "lesson_id" INT NOT NULL REFERENCES "lesson" ON DELETE CASCADE,
-    "lesson_level" VARCHAR(100) NOT NULL REFERENCES "level",
+    "lesson_level" "level" NOT NULL,
     "minimum_nr_of_students" INT NOT NULL,
     "maximum_nr_of_students" INT NOT NULL,
     "instrument" VARCHAR(100)
@@ -180,5 +138,5 @@ CREATE TABLE "group_lesson" (
 CREATE TABLE "individual_lesson" (
     "lesson_id" INT NOT NULL REFERENCES "lesson" ON DELETE CASCADE,
     "instrument" VARCHAR(100),
-    "level_id" INT NOT NULL REFERENCES "level"
+    "lesson_level" "level" NOT NULL  
 );
