0e4b4df115280dcef7e9eba908920fb37f8076f7
NataliAlhallak <132365555+NataliAlhallak@users.noreply.github.com>
2023-12-01 13:44:26 +0100

    Add files via upload

---
+++ person_data.sql
@@ -0,0 +1,171 @@
+INSERT INTO person (first_name, last_name, person_number, zipcode, street, city)
+VALUES
+  ('Oscar', 'Russell', '198189163838', '23664', '5452 Ornare, Ave', 'Overland Park'),
+  ('Lee', 'Norris', '194423904400', '77233', 'P.O. Box 300, 9162 Sociosqu Rd.', 'Lewiston'),
+  ('Igor', 'Alvarez', '196913737797', '46847', '419-962 Turpis. Road', 'Columbus'),
+  ('Hedley', 'Schmidt', '197856996213', '79532', '911-8712 Ridiculus Rd.', 'Omaha'),
+  ('Shelby', 'Munoz', '195460449675', '68427', '353-9785 Eu Road', 'New Haven'),
+  ('Ian', 'Pittman', '196925461512', '28161', '432-7336 Mollis. Rd.', 'Fort Smith'),
+  ('Wing', 'Schultz', '194296509124', '08489', '348-3701 Magna Rd.', 'Vancouver'),
+  ('Abra', 'Burnett', '195729252856', '46243', '642-2114 Dolor, Rd.', 'Frederick'),
+  ('Willa', 'Bernard', '198525676809', '82498', '607-6270 Quis Street', 'Overland Park'),
+  ('Quemby', 'Rice', '196255847108', '31441', 'Ap #753-3593 Libero Ave', 'Austin');
+INSERT INTO person (first_name, last_name, person_number, zipcode, street, city)
+VALUES
+  ('Neil', 'Morrow', '197869121839', '04284', '385-7310 Ultrices St.', 'Tacoma'),
+  ('Omar', 'Murray', '196861774203', '59358', '998-7752 Aliquet, Rd.', 'Salem'),
+  ('Summer', 'Solis', '198687409828', '46244', '611-8546 Cras Av.', 'Erie'),
+  ('Jarrod', 'Rosales', '196807037893', '85697', 'P.O. Box 291, 1831 Imperdiet Ave', 'Anchorage'),
+  ('Myles', 'Sweet', '197257958664', '76241', '9432 Sem Street', 'Fort Worth'),
+  ('Serena', 'Chavez', '199306154815', '17251', 'P.O. Box 658, 9904 Cum Ave', 'Milwaukee'),
+  ('Rama', 'Collins', '197582049009', '73865', 'Ap #688-2078 Tristique Ave', 'Cleveland'),
+  ('Eugenia', 'Pratt', '194944338580', '11415', '795-6628 A, Rd.', 'Bozeman'),
+  ('Chancellor', 'Callahan', '193294793893', '95162', '358-3673 In, St.', 'Rock Springs'),
+  ('Melyssa', 'Sargent', '193924822921', '78605', 'Ap #252-767 Mus. Ave', 'Pittsburgh');
+
+INSERT INTO person (first_name,last_name,person_number,zipcode,street,city)
+VALUES
+  ('Kay','Holland','200081236807','19130','P.O. Box 127, 2201 Mauris St.','Mesa'),
+  ('Laith','Mcclain','200452994723','84583','713-3951 Elit Rd.','Salt Lake City'),
+  ('Dante','Blevins','200975932911','63747','802-661 Proin Road','Broken Arrow'),
+  ('Hamilton','Flores','200937372851','18154','3603 Imperdiet St.','Tacoma'),
+  ('Jesse','Roy','200987589707','50984','Ap #481-7368 Sem St.','Knoxville'),
+  ('Kennedy','Clements','200162717966','35665','3931 In St.','Fort Wayne'),
+  ('Geraldine','Blackburn','200617553746','74824','614-9234 Nunc St.','Denver'),
+  ('Galvin','Nelson','200889346520','79112','Ap #679-6136 Eros. Av.','Richmond'),
+  ('Eugenia','Francis','200881978227','81433','Ap #630-1349 Malesuada Av.','Lawton'),
+  ('Mark','Serrano','200081901322','44471','Ap #661-5294 Tincidunt Avenue','Springfield');
+INSERT INTO person (first_name,last_name,person_number,zipcode,street,city)
+VALUES
+  ('Fletcher','Mooney','200628228824','08529','Ap #869-911 Amet Av.','Lansing'),
+  ('Basia','Strong','200108958684','60257','Ap #656-1089 Elit St.','Lafayette'),
+  ('Leroy','Page','200728816510','81185','P.O. Box 359, 9582 Ac, Avenue','Salem'),
+  ('Reece','Small','200223878454','04361','838-7071 Euismod Avenue','Auburn'),
+  ('Henry','Hancock','200637078897','13367','P.O. Box 567, 3497 Nunc. St.','Akron'),
+  ('Penelope','Bullock','200413705820','45882','Ap #862-4168 Egestas Road','Shreveport'),
+  ('Brent','Copeland','200698706206','34309','Ap #670-8598 Cursus, Ave','Pike Creek'),
+  ('Kiara','Gallagher','200812448869','44954','240-8160 Vitae St.','Fairbanks'),
+  ('Hayfa','Coleman','200082924772','38068','5585 Hendrerit. Av.','Iowa City'),
+  ('Graiden','Johnston','200063323115','81068','156-9107 Auctor Ave','Minneapolis');
+INSERT INTO person (first_name,last_name,person_number,zipcode,street,city)
+VALUES
+  ('Veronica','Hinton','200387778807','43534','Ap #474-4518 Sollicitudin Rd.','Broken Arrow'),
+  ('Justin','Massey','200235886611','54075','P.O. Box 554, 6855 Orci, St.','Newport News'),
+  ('Jacob','Aguirre','200836360161','31528','Ap #888-3302 Nulla St.','Fort Worth'),
+  ('Octavius','Holmes','200446050000','24954','Ap #952-6265 Ligula. Ave','Savannah'),
+  ('Bruno','Suarez','200130993071','54847','334-5426 Nullam Ave','Lawton'),
+  ('Hayden','Beach','200137957149','94857','100 Egestas Street','Missoula'),
+  ('Phoebe','Merritt','200040627245','38635','Ap #864-1603 Elit, Road','Kapolei'),
+  ('Reuben','Wilson','200162617326','77822','373-5449 Tempor St.','Savannah'),
+  ('Lucas','Shields','200244786991','52270','684-5459 Felis. Av.','Richmond'),
+  ('Flavia','Livingston','200890614266','65639','824-8417 Curabitur Avenue','Columbia');
+INSERT INTO person (first_name,last_name,person_number,zipcode,street,city)
+VALUES
+  ('Carolyn','Pate','200128698229','82787','8077 Ornare Street','Springfield'),
+  ('Nita','Griffith','200154734241','29124','3542 Erat. Rd.','Overland Park'),
+  ('Fiona','Glenn','200703998292','44264','Ap #900-3992 Mollis Road','Rockville'),
+  ('Mannix','Atkins','200873763716','62415','901-7383 Ac Rd.','Milwaukee'),
+  ('Hamilton','Rocha','200028770855','66124','P.O. Box 626, 8100 Molestie Rd.','Akron'),
+  ('Kareem','Goodwin','200107871862','65453','956-7656 Nunc Rd.','Grand Island'),
+  ('Thomas','Vaughn','200816017842','88572','P.O. Box 468, 1102 Nec, Street','Minneapolis'),
+  ('Leo','Ayala','200851725825','63874','P.O. Box 752, 7524 Sagittis Ave','Grand Rapids'),
+  ('Keaton','Cochran','200976490730','65325','Ap #710-7138 Tincidunt, Rd.','Chesapeake'),
+  ('Stephen','Hawkins','200503182487','38557','263-6863 Nulla St.','Billings');
+INSERT INTO person (first_name,last_name,person_number,zipcode,street,city)
+VALUES
+  ('Risa','Coffey','200995853368','17253','2604 In Ave','Portland'),
+  ('Luke','Warner','200823862633','33816','Ap #632-6113 Placerat, St.','Wilmington'),
+  ('Kim','Hernandez','200779574385','40735','716-6174 Eu, Road','Racine'),
+  ('Ian','Blair','200432619033','43270','Ap #584-2969 Ut Av.','Fort Worth'),
+  ('Olivia','Stevenson','200458251936','55700','1384 Nam Avenue','Oklahoma City'),
+  ('Marshall','Horton','200469860004','82379','371-363 Augue Avenue','Great Falls'),
+  ('Quynn','Miranda','200424470947','86786','Ap #934-7043 Fames St.','Waterbury'),
+  ('Nina','Davenport','200252109448','36766','925-360 Nulla Rd.','Bridgeport'),
+  ('Geraldine','Avery','200458039802','15818','P.O. Box 732, 8011 Elit, Rd.','Memphis'),
+  ('Adrienne','Blake','200670894458','69420','485-1975 Condimentum Avenue','Racine');
+INSERT INTO person (first_name,last_name,person_number,zipcode,street,city)
+VALUES
+  ('Chase','Reed','200654191670','41645','908-7990 Gravida Avenue','Rockford'),
+  ('Owen','Schroeder','200214976458','33546','727-7126 Nec Rd.','Detroit'),
+  ('Bo','Mcknight','200214044172','06312','598-9390 Ullamcorper, Rd.','Nampa'),
+  ('Keith','Bray','200627365545','40244','7594 Rutrum St.','Portland'),
+  ('Serina','Campos','200128729071','52633','P.O. Box 795, 7647 Elit St.','Green Bay'),
+  ('Ryder','Shaw','200808764852','85348','Ap #735-688 Semper Road','Salem'),
+  ('Bianca','Carroll','200084082601','17136','5024 Mattis Road','Rockford'),
+  ('Aimee','Osborne','200671320730','21819','970-4006 Diam Rd.','Boston'),
+  ('Calvin','Abbott','200709167009','37865','879-2673 In St.','Rockville'),
+  ('Kyla','Richard','200037138235','19332','P.O. Box 887, 3259 Eu Avenue','Bellevue');
+INSERT INTO person (first_name,last_name,person_number,zipcode,street,city)
+VALUES
+  ('Pascale','Golden','200896944684','73495','6617 Ornare, Ave','Rutland'),
+  ('Chaim','Maddox','200992168435','14386','206-9612 Vulputate, Street','Eugene'),
+  ('Garrett','Sosa','200670868481','21113','P.O. Box 894, 1706 Fusce Rd.','Georgia'),
+  ('Tatiana','Huber','200514379959','36343','P.O. Box 137, 259 Felis Rd.','Casper'),
+  ('Joseph','Mccormick','200528354197','78581','1536 Lacus. St.','Joliet'),
+  ('Dane','Stout','200962449266','82481','2951 Nulla St.','Sterling Heights'),
+  ('Gemma','Hopper','200416276706','34747','Ap #625-2626 Vitae, Rd.','Grand Island'),
+  ('Liberty','Rivers','200297700025','46464','Ap #998-1912 Facilisis Street','Idaho Falls'),
+  ('Cole','Cunningham','200075801278','41056','3925 Orci, St.','San Diego'),
+  ('Ann','Guzman','200941369973','80166','Ap #447-1039 Erat Rd.','Fort Collins');
+
+
+INSERT INTO person (first_name,last_name,person_number,zipcode,street,city)
+VALUES
+  ('Burke','Vinson','199875177497','59145','349-9439 Ligula Ave','Oklahoma City'),
+  ('Kiayada','Jenkins','200001814733','91934','Ap #196-6610 Leo Av.','Butte'),
+  ('Sydnee','Morin','199370498120','91272','Ap #312-4534 Vestibulum Ave','Southaven'),
+  ('Jin','Daniels','199220828928','76364','841-9337 Vehicula Road','Kailua'),
+  ('Ahmed','Foreman','199389879142','30453','388-5540 Mattis. Rd.','Reading'),
+  ('Yuli','Burch','199878541852','24633','304-4101 Eleifend, Rd.','Cleveland'),
+  ('Paloma','Holmes','199030900154','49941','Ap #426-5382 Vehicula. St.','Pocatello'),
+  ('Harrison','Slater','199016954511','56138','4574 Est Ave','Athens'),
+  ('Quynn','Parker','199138165460','03317','Ap #617-3287 Nisi Rd.','Pocatello'),
+  ('Ali','Reese','199861073696','35754','Ap #383-1008 Semper Avenue','Topeka');
+INSERT INTO person (first_name,last_name,person_number,zipcode,street,city)
+VALUES
+  ('Kenyon','Bishop','199196184008','26996','443-6055 Odio, Rd.','College'),
+  ('Grant','Burton','199462099622','38733','Ap #450-5889 Metus Rd.','Topeka'),
+  ('Scott','Fitzpatrick','199413739279','10670','P.O. Box 408, 4370 A, Rd.','Saint Louis'),
+  ('Zeph','Francis','199883430227','53317','Ap #653-753 Fusce Avenue','Pocatello'),
+  ('Levi','Dyer','199442377921','25656','175-1153 Nunc Avenue','Knoxville'),
+  ('Kirestin','Hendrix','199574823586','97246','499-9803 Nisl. St.','Fort Wayne'),
+  ('Neville','Wolf','199649633140','68777','P.O. Box 968, 9458 Justo Rd.','Houston'),
+  ('Kylynn','Sampson','199639878245','78566','Ap #571-853 Ac Rd.','New Orleans'),
+  ('Phyllis','Dillon','199179753138','03568','P.O. Box 596, 868 Aliquam, St.','Sandy'),
+  ('Warren','Puckett','199292679759','66001','2614 Nunc, Rd.','Sacramento');
+INSERT INTO person (first_name,last_name,person_number,zipcode,street,city)
+VALUES
+  ('Vanna','Harrison','199552269170','63665','P.O. Box 580, 3594 Magnis St.','Green Bay'),
+  ('Lewis','Lindsay','199688573009','01783','9404 Suspendisse Street','Vancouver'),
+  ('Sybil','Mccray','199436654592','31121','3915 Congue Street','Topeka'),
+  ('Quinn','Caldwell','199431817749','43957','Ap #449-7895 Montes, Ave','Gary'),
+  ('Omar','Charles','199231793304','12918','Ap #592-9500 Adipiscing St.','Tucson'),
+  ('Katell','Garza','199914042308','30447','955-734 Ipsum Ave','Clarksville'),
+  ('Kelsie','Valentine','200013054527','16454','P.O. Box 788, 2283 At, Ave','Shreveport'),
+  ('Dennis','Lara','199419981540','91976','P.O. Box 258, 4531 Egestas Street','Sandy'),
+  ('Maia','Santos','199892938406','91375','516-6637 Lorem St.','Wyoming'),
+  ('Piper','Santos','199822197522','73280','P.O. Box 194, 9648 Purus St.','Overland Park');
+INSERT INTO person (first_name,last_name,person_number,zipcode,street,city)
+VALUES
+  ('Geoffrey','Johnston','199496074726','98407','298-2501 Lectus Rd.','Gresham'),
+  ('Nigel','Wright','199532481285','58556','974-2114 Mollis. Road','Sandy'),
+  ('Nerea','Marks','199914795989','15939','Ap #431-5388 Laoreet Street','Bear'),
+  ('Nolan','Reilly','199888517743','27831','P.O. Box 741, 1992 Augue Av.','Columbia'),
+  ('Akeem','Fischer','199827297999','27847','294-5092 Fermentum Avenue','Rockford'),
+  ('Kirsten','Noble','199522741855','30553','778-7974 Lorem Avenue','Saint Louis'),
+  ('Shelby','Rutledge','199091332108','15783','365-8302 Enim Road','Fayetteville'),
+  ('Bernard','Gibson','199180161901','77332','Ap #165-7413 Scelerisque Rd.','Columbus'),
+  ('Beverly','Hampton','199423276401','22507','Ap #509-2383 Aliquet Rd.','Chattanooga'),
+  ('Lynn','Howe','199745489777','52251','8490 Vehicula. Ave','Springdale');
+INSERT INTO person (first_name,last_name,person_number,zipcode,street,city)
+VALUES
+  ('Perry','Richards','199919171648','95243','107-4981 Cras St.','Los Angeles'),
+  ('Rebecca','Mitchell','199014490817','10807','964-6251 Lacus. Rd.','Portland'),
+  ('Nicole','Johnson','199562540066','19373','536 Aliquam Road','Stamford'),
+  ('Lucas','Carroll','199044267391','06771','P.O. Box 296, 2807 Vulputate, Rd.','Gresham'),
+  ('Sybill','Avila','199280724189','19346','Ap #678-4247 Blandit St.','Savannah'),
+  ('Ian','Rojas','199441338450','96382','Ap #412-7029 Donec St.','Portland'),
+  ('Tatiana','Everett','199268838354','25031','207-2794 Suspendisse St.','Paradise'),
+  ('Astra','Dillon','199987204009','75796','Ap #559-144 Fermentum Av.','Richmond'),
+  ('Connor','Francis','199330528756','41162','824 Donec Avenue','Hartford'),
+  ('Sawyer','Berger','199917861613','45539','Ap #280-5777 Est, Rd.','Pittsburgh');
